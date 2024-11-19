2bc8c5df390a8b8c9ce87ce365ba667deeb607e4
leopilots <leonardoyalejandrosr@gmail.com>
2024-11-19 14:23:22 -0400

    js

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+function suma(){
+    let numeroUno = 5;
+    let numeroDos = 7;
+    let numeroTres = 9;
+    let resultado= numeroUno+numeroDos+numeroTres;
+    alert("La suma es "+resultado);
+}
+function resta(){
+    let numeroUno = 5;
+    let numeroDos = 7;
+    let numeroTres = 9;
+    let resultado= numeroUno-numeroDos-numeroTres;
+    alert("La resta es "+resultado);
+}
+function multiplicacion(){
+    let numeroUno = 5;
+    let numeroDos = 7;
+    let numeroTres = 9;
+    let resultado= numeroUno*numeroDos*numeroTres;
+    alert("La multiplicacion es "+resultado);
+}
+function division(){
+    let numeroUno = 5;
+    let numeroDos = 7;
+    let numeroTres = 9;
+    let resultado= numeroUno/numeroDos/numeroTres;
+    alert("La division es "+resultado);
+}
